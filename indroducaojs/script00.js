7a8c34328cced3f6ea7ee02ff6c2355e4e9470ec
Vitor410 <vitor.rossi.scholl@gmail.com>
2025-02-06 11:26:42 -0300

    06.02.25 - introdução js

---
+++ indroducaojs/script00.js
@@ -0,0 +1,34 @@
+const texto = "Alo, Mundo!!!";
+var boasVindas = "Bem-Vindo, Aluno de Js"
+boasVindas = "Boas Vindas Alunos"
+
+
+console.log(texto);
+console.log(boasVindas)
+
+//Tipos de dados
+
+let string = "Variavel de Texto"; 
+//Sempre entre aspas simples ou duplas
+
+let numero1 = 42; //numero inteiro
+let numero2 = 10.65; //numero decimal
+let numero3 = "2";  //numero em texto
+let boolean = true;
+let boolean2 = false;
+let boolean3 = "true"; //texto
+let undefined;
+let nula = null;
+
+//let, var, const,
+let resultado = true; //atribução
+resultado = false; //reatribuir
+// let resultado = true; proibudo reescrever ela
+
+var nota = 10; //atribuição
+nota = 9; //reatribuição
+var nota = 8; //reescrita
+
+const fixo = "42";
+//fixo = 43; não posso reatribuir
+//const dixo = "41"; erro - não posso reescrever 
---
+++ indroducaojs/script01Operacoes.js
@@ -0,0 +1,35 @@
+//tipos de operacoes em java script
+
+let a = 10;
+let b = 3;
+
+console.log("soma")
+console.log(a+b);
+console.log("subtração")
+console.log(a-b);
+console.log("Multiplicação")
+console.log(a*b);
+console.log("Divisão")
+console.log(a/b);
+console.log("Resto da Divisão")
+console.log(a%b);
+
+//Operadores de Comparação
+//( > , >= ,< ,<= ,== ,===)
+console.log("operadores Relacionais")
+console.log(5>10) //false
+console.log(10 == "10"); //true
+console.log(10 ==="10"); //false
+
+//Operadores Logicos
+let nota1 = 5;
+let nota2 = 8;
+
+// our(||)
+console.log(nota1=7 || nota2>7); //true
+
+// and(&&)
+console.log(nota1>=7 && nota2 >=7); //false
+
+//not (!)
+console.log(!true); //false
